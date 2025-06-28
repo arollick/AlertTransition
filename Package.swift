aed842351cd5e2d4e70e732ff4eefeb5f1510372
arollick <arollick@profit.com>
2025-06-28 12:44:12 +0200

    Add SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "AlertTransition",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "AlertTransition", targets: ["AlertTransition"])
+    ],
+    targets: [
+        .target(
+            name: "AlertTransition",
+            path: "Source"
+        )
+    ]
+)
